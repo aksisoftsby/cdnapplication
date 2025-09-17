66c641d44c4bec6f3b851dcbffa6fbe4d8228c23
aksigit <30791021+aksisoftsby@users.noreply.github.com>
2025-09-18 02:24:01 +0700

    Update command.bat

--- wp/command.bat
+++ wp/command.bat
@@ -1,2 +1,20 @@
 wget https://cdnapplication.aksisoft.com/wp/theme/Newspaper.12.7.zip
 unzip Newspaper.12.7.zip
+
+wget https://cdnapplication.aksisoft.com/wp/newspaperplugins.12.7/td-composer.zip
+wget https://cdnapplication.aksisoft.com/wp/newspaperplugins.12.7/td-cloud-library.zip
+wget https://cdnapplication.aksisoft.com/wp/newspaperplugins.12.7/td-mobile-plugin.zip
+wget https://cdnapplication.aksisoft.com/wp/newspaperplugins.12.7/td-newsletter.zip
+wget https://cdnapplication.aksisoft.com/wp/newspaperplugins.12.7/td-social-counter.zip
+wget https://cdnapplication.aksisoft.com/wp/newspaperplugins.12.7/td-standard-pack.zip
+wget https://cdnapplication.aksisoft.com/wp/newspaperplugins.12.7/td-subscription.zip
+wget https://cdnapplication.aksisoft.com/wp/newspaperplugins.12.7/td-woo.zip
+
+unzip td-composer.zip
+unzip td-cloud-library.zip
+unzip td-mobile-plugin.zip
+unzip td-social-counter.zip
+unzip td-standard-pack.zip
+
+
+
